52819d260a7d50a53f757e6baf5fef533621a1cb
Ruel <c0407h@gmail.com>
2024-03-31 13:08:05 +0900

    Initial Commit

---
+++ SwiftUI_DiarySample/SwiftUI_DiarySampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_DiarySampleApp.swift
+//  SwiftUI_DiarySample
+//
+//  Created by Chung Wussup on 3/31/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_DiarySampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
