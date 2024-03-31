4f6d369b29b01a07878031fc035ae34264384429
Ruel <c0407h@gmail.com>
2024-03-31 13:09:41 +0900

    init commit

---
+++ SwiftUI_DiarySample/DiaryList/DiaryListView.swift
@@ -0,0 +1,18 @@
+//
+//  DiaryListView.swift
+//  SwiftUI_DiarySample
+//
+//  Created by Chung Wussup on 3/31/24.
+//
+
+import SwiftUI
+
+struct DiaryListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DiaryListView()
+}
